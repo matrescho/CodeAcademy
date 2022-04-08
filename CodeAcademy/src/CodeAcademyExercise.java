2820d383287d3ab2be1ac6ae1cb780b19bfc7424
matrescho <matres3@abv.bg>
2022-04-08 14:01:22 +0300

    exercise

--- CodeAcademy/src/CodeAcademyExercise.java
+++ CodeAcademy/src/CodeAcademyExercise.java
@@ -5,6 +5,7 @@ public class CodeAcademyExercise {
 
     public static void vowels () {
 
+
         /*▪ Ȁрябва да си купите нови маратонки. Оглеждате офертите, но имате
 няколко изисквания:
 1. Цената им не трябва да е над 150 лв.;
@@ -16,7 +17,7 @@ public class CodeAcademyExercise {
 
 
      */
-        Scanner scanner = new Scanner(System.in);
+       Scanner scanner = new Scanner(System.in);
         int i = 0;
 
         while (i < 100000) {
@@ -273,6 +274,304 @@ public class CodeAcademyExercise {
 
     }
 
+    public static void cinemaTickets(){
+
+
+
+
+        /*4. 7-членно семейство отива на кино. Като двама от тях са пенсионери, двама възрастни,
+         един студент и един непълнолетен и едно дете на 11 години.
+ Отстъпките в киното:
+За пенсионери - 10%
+За студенти - 30%
+За ученици - 50%
+За деца е безплатно (под 7 години)
+Колко ще е общата сума която ще плати семейство?
+
+         */
+
+        //Един вариянт на задачката и е точно все едно си на касата задават се 2
+        //вида на билета и неговата бройка
+        Scanner scanner = new Scanner(System.in);
+        String command = "";
+
+        int ticketPrice = 12;
+        double totalTicketPrice = 0;
+        int numberOfTickets =0;
+        int totalTicketsBought = 0;
+
+
+        while (! command.equalsIgnoreCase("Stop")){
+
+            System.out.println("Enter ticket kind");
+            command = scanner.nextLine();
+
+            if (command.equalsIgnoreCase("Elderly")){
+                System.out.println("Enter number of tickets");
+                numberOfTickets = scanner.nextInt();
+                totalTicketPrice += (ticketPrice * numberOfTickets) * 0.90;
+                totalTicketsBought += numberOfTickets;
+
+
+            }else if (command.equalsIgnoreCase("collage Student")){
+                System.out.println("Enter number of tickets");
+                numberOfTickets = scanner.nextInt();
+                totalTicketPrice += (ticketPrice * numberOfTickets) * 0.70;
+                totalTicketsBought += numberOfTickets;
+
+
+            }else if (command.equalsIgnoreCase("Students")){
+                System.out.println("Enter number of tickets");
+                numberOfTickets = scanner.nextInt();
+                totalTicketPrice += (ticketPrice * numberOfTickets) * 0.50;
+                totalTicketsBought += numberOfTickets;
+
+            }else if (command.equalsIgnoreCase("regular")){
+                System.out.println("Enter number of tickets");
+                numberOfTickets = scanner.nextInt();
+                totalTicketPrice += ticketPrice * numberOfTickets;
+                totalTicketsBought += numberOfTickets;
+
+            }else if (command.equalsIgnoreCase("Children")){
+                System.out.println("Enter number of tickets");
+                numberOfTickets = scanner.nextInt();
+                ticketPrice = 0;
+                totalTicketsBought += numberOfTickets;
+            }
+
+        }
+
+        System.out.printf("You bought " + totalTicketsBought + " tickets for the price of %.2flv.",totalTicketPrice);
+
+
+
+
+    }
+
+    public static void traditionalSalad(){
+
+        /*3. Искате да си направите шопска салата. Трябват ви домати, краставици, лук, чушки, сирене:
+Цената на продуктите
+Домати - 7лв/кг
+Краставици - 6лв/кг
+Чушки-5лв/кг
+Лук-3лв/кг
+Сирене-15лв/кг
+Имате 20лв колко салата може да си направите?
+//Оцет, олио и сол си имате вкъщи
+
+         */
+        Scanner scanner = new Scanner(System.in);
+        double totalMoney =20;
+
+        double cheese = 15;
+        double onion = 3;
+        double tomato = 7;
+        double cucumber = 6;
+        double peppers = 5;
+
+
+        //за сега не знам дали е възможно да го използвам в While loop и ако е как точно става..
+     //   enum vegetables {
+
+   //      CHEESE(15),
+   //      ONION(3),
+   //      TOMATO(7),
+   //      CUCUMBER(6),
+   //      PEPPERS(5);
+
+   //      double price ;
+   //      vegetables(double price){
+   //          this.price = price;
+   //      }
+
+   //  }
+
+        String command = "";
+
+
+        while (!command.equalsIgnoreCase("stop")){
+
+            String Vegetables;
+            double quantity = 0;
+            double priceForTheseVegetables = 0;
+
+
+            System.out.println("\nEnter the vegetable u want to purchase");
+            command = scanner.nextLine();
+
+            if (totalMoney <=0){
+                break;
+            }
+
+            if (command.equalsIgnoreCase("peppers")){
+                System.out.println("Enter quantity of peppers");
+                quantity = scanner.nextDouble();
+                priceForTheseVegetables = (quantity * 5);
+                totalMoney -= priceForTheseVegetables;
+                System.out.printf("You bought " + quantity + " Of peppers for the price of %.2flv.\nu have %.2flv. left"
+                ,priceForTheseVegetables,(totalMoney-priceForTheseVegetables));
+
+            }else if (command.equalsIgnoreCase("cucumber")){
+                System.out.println("Enter quantity of cucumber");
+                quantity = scanner.nextDouble();
+                priceForTheseVegetables = (quantity * 6);
+                totalMoney -= priceForTheseVegetables;
+                System.out.printf("You bought " + quantity + " Of cucumber for the price of %.2flv.\nu have %.2flv. left"
+                        ,priceForTheseVegetables,(totalMoney-priceForTheseVegetables));
+
+            }else if (command.equalsIgnoreCase("onion")){
+                System.out.println("Enter quantity of onion");
+                quantity = scanner.nextDouble();
+                priceForTheseVegetables = (quantity * 3);
+                totalMoney -= priceForTheseVegetables;
+                System.out.printf("You bought " + quantity + " Of onion for the price of %.2flv.\nu have %.2flv. left"
+                        ,priceForTheseVegetables,(totalMoney-priceForTheseVegetables));
+
+            }else if (command.equalsIgnoreCase("tomato")){
+                System.out.println("Enter quantity of tomato");
+                quantity = scanner.nextDouble();
+                priceForTheseVegetables = (quantity * 7);
+                totalMoney -= priceForTheseVegetables;
+                System.out.printf("You bought " + quantity + " Of tomato for the price of %.2flv.\nu have %.2flv. left"
+                        ,priceForTheseVegetables,(totalMoney-priceForTheseVegetables));
+
+            }else if (command.equalsIgnoreCase("cheese")){
+                System.out.println("Enter quantity of cheese");
+                quantity = scanner.nextDouble();
+                priceForTheseVegetables = (quantity * 15);
+                totalMoney -= priceForTheseVegetables;
+                System.out.printf("You bought " + quantity + " Of cheese for the price of %.2flv.\nu have %.2flv. left"
+                        ,priceForTheseVegetables,(totalMoney-priceForTheseVegetables));
+
+            }
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+    }//ТРЯБВА ПОДОБРЕНИЕ !!!!
+
+    public static void traditionalSaladVersion2() {
+        /*3. Искате да си направите шопска салата. Трябват ви домати, краставици, лук, чушки, сирене:
+Цената на продуктите
+Домати - 7лв/кг
+Краставици - 6лв/кг
+Чушки-5лв/кг
+Лук-3лв/кг
+Сирене-15лв/кг
+Имате 20лв колко салата може да си направите?
+//Оцет, олио и сол си имате вкъщи
+
+         */
+
+         Scanner scanner = new Scanner(System.in);
+
+
+
+            double totalMoney = 20;
+
+            double cheese = 15.0;
+            double onion = 3.0;
+            double tomato = 7.0;
+            double cucumber = 6.0;
+            double peppers = 5.0;
+
+            double quantityOfCheese = 0;
+            double quantityOfOnion = 0;
+            double quantityOfTomato = 0;
+            double quantityOfCucumber = 0;
+            double quantityOfPeppers = 0;
+
+            String command = "";
+            double quantity = 0;
+            double totalPriceForVegetable = 0;
+            System.out.println("Hello we sell Cheese,peppers,onions,cucumbers,tomato's what would u like to buy");
+
+            while (!command.equalsIgnoreCase("Stop")) {
+
+                command = scanner.nextLine();
+
+                if (command.equalsIgnoreCase("cheese")) {
+                    System.out.println("Enter the quantity of cheese u want to buy");
+                    quantity = scanner.nextDouble();
+                    totalPriceForVegetable = (quantity * cheese);
+                    totalMoney -= totalPriceForVegetable;
+                    quantityOfCheese += quantity;
+                    continue;
+
+                } else if (command.equalsIgnoreCase("onion")) {
+                    System.out.println("Enter the quantity of onion u want to buy");
+                    quantity = scanner.nextDouble();
+                    totalPriceForVegetable = (quantity * onion);
+                    totalMoney -= totalPriceForVegetable;
+                    quantityOfOnion += quantity;
+                    continue;
+
+                } else if (command.equalsIgnoreCase("tomato")) {
+                    System.out.println("Enter the quantity of tomato's u want to buy");
+                    quantity = scanner.nextDouble();
+                    totalPriceForVegetable = (quantity * tomato);
+                    totalMoney -= totalPriceForVegetable;
+                    quantityOfTomato += quantity;
+                    continue;
+
+                } else if (command.equalsIgnoreCase("cucumber")) {
+                    System.out.println("Enter the quantity of cucumbers u want to buy");
+                    quantity = scanner.nextDouble();
+                    totalPriceForVegetable = (quantity * cucumber);
+                    totalMoney -= totalPriceForVegetable;
+                    quantityOfCucumber += quantity;
+                    continue;
+
+                } else if (command.equalsIgnoreCase("peppers")) {
+                    System.out.println("Enter the quantity of peppers u want to buy");
+                    quantity = scanner.nextDouble();
+                    totalPriceForVegetable = (quantity * peppers);
+                    totalMoney -= totalPriceForVegetable;
+                    quantityOfPeppers += quantity;
+                    continue;
+                }
+                if (!command.equalsIgnoreCase("Stop")) {
+                    double canBuyCheese = totalMoney / cheese;
+                    double canBuyOnion = totalMoney / onion;
+                    double canBuyTomato = totalMoney / tomato;
+                    double canBuyCucumber = totalMoney / cucumber;
+                    double canBuyPeppers = totalMoney / peppers;
+
+                    System.out.println("You have " + totalMoney + "lv. left u can buy ");
+                    System.out.printf("%.3f kilograms of cheese%n", canBuyCheese);
+                    System.out.printf("%.3f kilograms of onion%n", canBuyOnion);
+                    System.out.printf("%.3f kilograms of tomato%n", canBuyTomato);
+                    System.out.printf("%.3f kilograms of cucumber%n", canBuyCucumber);
+                    System.out.printf("%.3f kilograms of peppers%n", canBuyPeppers);
+
+
+                    System.out.println("Hello we sell Cheese,peppers,onions,cucumbers,tomato's what would u like to buy");
+                }
+
+            }
+            double totalSaladMade = quantityOfCheese + quantityOfCucumber + quantityOfOnion
+                    + quantityOfPeppers + quantityOfTomato;
+            System.out.printf("\n%.3f you bought kilograms of cheese\n", quantityOfCheese);
+            System.out.printf("%.3f you bought kilograms of onion\n", quantityOfOnion);
+            System.out.printf("%.3f you bought kilograms of tomato\n", quantityOfTomato);
+            System.out.printf("%.3f  you bought kilograms of cucumber\n", quantityOfCucumber);
+            System.out.printf("%.3f you bought kilograms of peppers\n", quantityOfPeppers);
+
+            System.out.printf("\nYou have %.2flv. left and u can make %.3f grams of traditional salad"
+                    , totalMoney, totalSaladMade);
+        }
+
 
 
 
@@ -282,6 +581,7 @@ public class CodeAcademyExercise {
 
 
     public static void main(String[] args) {
+
        // leapYear();
        // vowels();
        // shoes();
@@ -290,6 +590,9 @@ public class CodeAcademyExercise {
        // grade();
        // salaryBonus();
        // discountForCloths();
+       // cinemaTickets();
+        //traditionalSalad();
+        traditionalSaladVersion2();
 
 
 
